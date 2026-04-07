faaa2e07f3744f3f562f4ad12ed370a459ed771e
JaideepVerma <61654942+JaideepVerma@users.noreply.github.com>
2026-04-07 20:36:28 +0530

    Update ZSscrape.py
    added db setup in main

--- ZSscrape.py
+++ ZSscrape.py
@@ -109,7 +109,36 @@ cur.execute("ALTER TABLE jobs ADD COLUMN loaded_at TEXT;")
 conn.commit()
 conn.close() 
 '''
+def dbsetup():
+    
+conn = sqlite3.connect("ZSjobs.db")
+c = conn.cursor()
+
+c.execute('''
+CREATE TABLE IF NOT EXISTS jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company TEXT,
+    job_id TEXT,
+    role TEXT,
+    description TEXT,
+    responsibilities TEXT,
+    qualifications TEXT,
+    location TEXT,
+    posting_date TEXT,
+    job_family TEXT,
+    job_function TEXT,
+    apply_link TEXT,
+    posted_at TEXT,
+    loaded_at TEXT 
+          
+)
+''')
+
+conn.commit()
+conn.close()
+print("Jobs table updated successfully.")
 if __name__ == "__main__":
+    dbsetup()
     jobs = scrape_zs()
     save_jobs(jobs)
     print('Jobs saved to .db')
